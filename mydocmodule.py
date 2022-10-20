013aaa2f19d6101a2dd10a16745fe28e709d202f
Mike Starr <mstarr235@gmail.com>
2022-10-20 08:24:48 -0600

    first commit

---
+++ mydocmodule.py
@@ -0,0 +1,7 @@
+def sqFootageHouse(length, width):
+    house_area = length * width
+    return f'{house_area} is the square footage of your house'
+
+def calcCircumferenceCircle(radius):
+    circumference_circle = 2*3.14*radius
+    return f'{circumference_circle} is the circumference of your circle'
